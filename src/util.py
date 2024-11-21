6a8482227867c9e8da1938d9576706fad4745862
Vloon <pleunscholten@hotmail.com>
2024-11-21 21:37:13 +0100

    Remove mistake and test branches

--- src/util.py
+++ src/util.py
@@ -5,7 +5,7 @@ T = TypeVar("T")
 
 
 def create_all_translations(values: tuple[T, ...]) -> list[tuple[T, ...]]:
-    return [values[i:] + values[:i] for i in range(len(values)-1)]
+    return [values[i:] + values[:i] for i in range(len(values))]
 
 
 def get_minimal_translation(values: tuple[T, ...]) -> tuple[T, ...]:
