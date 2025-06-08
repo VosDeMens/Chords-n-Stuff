690da6547cf54221238b7bea9efa345f319e2d85
VosDeMens <vosdemens@gmail.com>
2025-06-08 17:38:37 +0200

    updated profiler

--- src/profiler.py
+++ src/profiler.py
@@ -7,7 +7,9 @@ from typing import Callable, Any, TypeVar, cast
 T = TypeVar("T", bound=Callable[..., Any])
 C = TypeVar("C", bound=type)
 
-class_timings: dict[type, dict[str, float]] = defaultdict(dict[str, float])
+class_timings: dict[type, dict[str, tuple[float, int]]] = defaultdict(
+    dict[str, tuple[float, int]]
+)
 
 
 def timed_method(cls: type) -> Callable[[T], T]:
@@ -38,8 +40,9 @@ def timed_method(cls: type) -> Callable[[T], T]:
             duration = perf_counter() - start
             method_name = method.__name__
             if method_name not in class_timings[cls]:
-                class_timings[cls][method_name] = 0
-            class_timings[cls][method_name] += duration
+                class_timings[cls][method_name] = (0, 0)
+            time, count = class_timings[cls][method_name]
+            class_timings[cls][method_name] = (time + duration, count + 1)
             return result
 
         return cast(T, wrapper)
@@ -68,7 +71,7 @@ class TimingMeta(ABCMeta):
     ) -> type:
         cls = super().__new__(mcs, name, bases, namespace)
         for attr_name, attr_value in namespace.items():
-            if callable(attr_value) and not attr_name.startswith("__"):
+            if callable(attr_value):  # and not attr_name.startswith("__"):
                 wrapped = timed_method(cls)(attr_value)
                 setattr(cls, attr_name, wrapped)
         return cls
@@ -85,12 +88,10 @@ def report_timings() -> None:
     After reporting, the stored timing data is cleared.
     """
     print("\nTiming Report:")
-    total = 0
-    for cls, t in sorted(class_timings.items(), key=lambda x: -sum(x[1].values())):
+    for cls, t in sorted(
+        class_timings.items(), key=lambda x: -sum([time for time, _ in x[1].values()])
+    ):
         print(f"{cls.__name__}:")
-        for k, v in t.items():
-            total += v
-            print(f"    {k}: {v:.6f}")
+        for k, (time, count) in sorted(t.items(), key=lambda x: -x[1][0]):
+            print(f"    {k}: {time:.6f} seconds, called {count} times")
     class_timings.clear()
-
-    print(f"Total: {total}")
